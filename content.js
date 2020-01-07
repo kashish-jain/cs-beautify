02e1621644f77a1431b1a2af34cd77d3f7f507bb
Kashish Jain <kashish.emailme@gmail.com>
2020-01-06 19:55:01 -0500

    dependencies updated; navbar somewhat better

--- content.js
+++ content.js
@@ -1,1 +1,34 @@
 document.getElementById("headerbox").style.color = "pink";
+
+let menubox =  document.getElementsByClassName("menubox")[0];
+let menuItems = menubox.getElementsByTagName("div");
+let thisPageMenu = document.getElementById("thispagemenu");
+let navbarParent = document.createElement('div');
+document.body.insertBefore(navbarParent, document.body.childNodes[2]);
+navbarParent.classList.add("navbar", "navbar-expand-lg", "navbar-light", "bg-light")
+navbarParent.appendChild(menubox);
+
+let newActiveElement = document.createElement('a');
+newActiveElement.classList.add("nav-link", "active");
+newActiveElement.innerHTML = thisPageMenu.innerHTML;
+thisPageMenu.innerHTML = "";
+thisPageMenu.appendChild(newActiveElement);
+
+// Clear all the styles that is added by #thisPageMenu
+thisPageMenu.style.border = "0px";
+thisPageMenu.style.padding = "0px";
+thisPageMenu.style.margin = "0px";
+
+menubox.classList.remove("menubox");
+menubox.classList.add("navbar-nav")
+
+// adding styles to menu items
+for(let i = 0; i < menuItems.length; ++i) {
+    menuItems[i].classList.remove("menulink");
+    menuItems[i].classList.add("nav-item");
+    let link = menuItems[i].getElementsByTagName('a')[0];
+    if(link) {
+        link.classList.remove("menulink");
+        link.classList.add('nav-link');
+    }
+}
--- eventPage.js
+++ eventPage.js
@@ -1,10 +1,10 @@
 function checkForValidUrl(tabId, changeInfo, tab) {
-    // If the tabs url starts with "http://specificsite.com"...
+    // If the tabs url starts with https://www.student.cs.uwaterloo.ca/~cs240/
     if (tab.url.indexOf('https://www.student.cs.uwaterloo.ca/~cs240/') == 0) {
         // ... show the page action.
         chrome.pageAction.show(tabId);
     }
-};  
+};
 
 // Listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
