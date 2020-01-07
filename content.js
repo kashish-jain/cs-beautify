d4501ef27d376d885e5b57184257ffe2342a5473
Kashish Jain <kashish.emailme@gmail.com>
2020-01-06 20:54:51 -0500

    navbar looks okay

--- content.js
+++ content.js
@@ -1,11 +1,21 @@
-document.getElementById("headerbox").style.color = "pink";
-
 let menubox =  document.getElementsByClassName("menubox")[0];
 let menuItems = menubox.getElementsByTagName("div");
 let thisPageMenu = document.getElementById("thispagemenu");
 let navbarParent = document.createElement('div');
 document.body.insertBefore(navbarParent, document.body.childNodes[2]);
-navbarParent.classList.add("navbar", "navbar-expand-lg", "navbar-light", "bg-light")
+navbarParent.classList.add("navbar", "navbar-expand-lg", "navbar-dark", "bg-dark");
+
+// Navbar brand
+let navbarBrand = document.createElement('a');
+let courseTitle = document.getElementById("coursetitle");
+navbarBrand.innerHTML = courseTitle.innerHTML;
+navbarBrand.classList.add('navbar-brand');
+navbarParent.appendChild(navbarBrand);
+courseTitle.parentElement.removeChild(courseTitle);
+
+// Creating nav
+menubox.classList.remove("menubox");
+menubox.classList.add("navbar-nav", "ml-auto");
 navbarParent.appendChild(menubox);
 
 let newActiveElement = document.createElement('a');
@@ -18,9 +28,13 @@ thisPageMenu.appendChild(newActiveElement);
 thisPageMenu.style.border = "0px";
 thisPageMenu.style.padding = "0px";
 thisPageMenu.style.margin = "0px";
+thisPageMenu.style.height = "0px";
 
-menubox.classList.remove("menubox");
-menubox.classList.add("navbar-nav")
+// Remove headerbox
+let headerBox = document.getElementById("headerbox");
+headerBox.parentElement.removeChild(headerBox);
+
+
 
 // adding styles to menu items
 for(let i = 0; i < menuItems.length; ++i) {
